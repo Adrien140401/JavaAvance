4d3f18ca318794717639d02ba379660df1a1da6a
AxelPerrin <91256042+AxelPerrin@users.noreply.github.com>
2023-05-05 14:14:05 +0200

    push comment

--- src/main/java/com/example/javaavance/controller/Hellocontroller.java
+++ src/main/java/com/example/javaavance/controller/Hellocontroller.java
@@ -1,5 +1,6 @@
 package com.example.javaavance.controller;
 
+// Importation des classes nécessaires
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,8 +23,10 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.json.*;
 
+// Définition de la classe Hellocontroller
 public class Hellocontroller {
 
+    // Injection de dépendances des éléments définis dans le fichier FXML
     @FXML
     private Label platsListLabel;
 
@@ -36,7 +39,9 @@ public class Hellocontroller {
     @FXML
     private Label platslistdesc;
 
+    // Méthode d'initialisation appelée lorsque le contrôleur est chargé
     public void initialize() {
+        // Lecture du fichier JSON contenant les données sur les plats
         String datajson = "[]";
         try {
             datajson = new String(Files.readAllBytes(Paths.get("./json/plat.json")));
@@ -44,14 +49,20 @@ public class Hellocontroller {
             throw new RuntimeException(e);
         }
 
+        // Transformation du contenu JSON en tableau d'objets JSON
         JSONArray myArray = new JSONArray(datajson);
+        // Création de listes pour stocker les noms de plats, les prix, les stocks et les descriptions
         List<String> plats = new ArrayList<>();
         List<String> prix1 = new ArrayList<>();
         List<String> stocks = new ArrayList<>();
         List<String> descs = new ArrayList<>();
+        // Parcours de chaque objet JSON du tableau
         myArray.forEach(object -> {
+            // Conversion de l'objet en objet JSON
             JSONObject myjsonobject = (JSONObject) object;
+            // Récupération de la valeur de stock de chaque plat
             int stock = myjsonobject.getInt("stock");
+            // Si le stock est supérieur à zéro, on ajoute le plat aux listes correspondantes, sinon on l'ajoute avec la mention "indisponible"
             if (stock > 0) {
                 plats.add(myjsonobject.getString("plat"));
                 prix1.add(String.valueOf(myjsonobject.getInt("prix")));
@@ -62,34 +73,51 @@ public class Hellocontroller {
             }
         });
 
+        // Concaténation des noms de plats pour les afficher dans l'interface graphique
         String platNames = plats.stream()
                 .reduce("", (acc, plat) -> acc + "\n" + plat);
         platsListLabel.setText(platNames);
 
+        // Calcul du prix total des plats sélectionnés pour les afficher dans l'interface graphique
         int prixPlat = Integer.parseInt(prix1.stream()
                 .reduce("", (acc, prix) -> acc + "\n" + prix)
                 .replaceAll("[^\\d]", ""));
         platslistprix.setText(prixPlat + " euros\n");
 
+        // Concaténation des descriptions de plats pour les afficher dans l'interface graphique
         String platdesc = descs.stream()
                 .reduce("", (acc, desc) -> acc + "\n" + "\n" + desc);
         platslistdesc.setText(platdesc);
 
+        // Concaténation des stocks de plats pour les afficher dans l'interface graphique
         int platstock = Integer.parseInt(stocks.stream()
-                .reduce("", (acc, stock) -> acc + "\n" + stock )
+                .reduce("", (acc, stock) -> acc + "\n" + stock)
                 .replaceAll("[^\\d]", ""));
         platsliststock.setText(platstock + "\n");
     }
 
-        private Stage stage;
-        private Scene scene;
-        private Parent root;
+    // Définition des variables pour la scène
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
+    /**
+     * Cette méthode permet de changer la scène affichée
+     *
+     * @param event événement de type ActionEvent
+     * @throws IOException si le chargement de la nouvelle scène échoue
+     */
     public void switchScene(ActionEvent event) throws IOException {
+        // On charge la nouvelle scène depuis le fichier FXML correspondant
         root = FXMLLoader.load(getClass().getResource("/com/example/javaavance/view/createplat.fxml"));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        // On récupère la fenêtre actuelle depuis l'événement
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // On crée une nouvelle scène avec le contenu chargé depuis le fichier FXML
         scene = new Scene(root);
 
+        // On affecte la nouvelle scène à la fenêtre et on l'affiche
         stage.setScene(scene);
         stage.show();
     }
